16da1a7234cdbb58ee1fabbdb257875aacdea718
Kumaresan <kumaresankp21@gmail.com>
2026-06-13 15:38:18 +0530

    Update ALLOWED_HOSTS to allow all hosts for development

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-finance-tracker-secret-key-change-in-production-20
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com','*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
